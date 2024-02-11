ad0b0afadd04ae7a2465adbbe2be8e7910a6b6a4
Gustavo Antunes Voltolini <gustavoant.voltolini@gmail.com>
2024-02-10 21:55:11 -0300

    Removing extra tags

--- app/admin/login/page.tsx
+++ app/admin/login/page.tsx
@@ -33,19 +33,19 @@ export default function AdminLogin({searchParams}: PageProps) {
       <form onSubmit={handleSubmit}>
         <label htmlFor="admin-username">Usuario:</label>
         <input type="text" name="username" 
+          required
           autoComplete="off"
           value={inputs.username || ""}
           onChange={handleChange}
-          required
-          id="admin-username" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="olharte" required/>
+          id="admin-username" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="olharte"/>
 
-        <label htmlFor="admin-username">Senha:</label>
-        <input type="text" name="password" id="admin-password"
+        <label htmlFor="admin-password">Senha:</label>
+        <input type="password" name="password" id="admin-password"
           autoComplete="off"
           required
           value={inputs.password || ""}
           onChange={handleChange}
-          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="********" required/>
+          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="********" />
 
         <button type="submit" 
         className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 mt-4 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">Entrar</button>
